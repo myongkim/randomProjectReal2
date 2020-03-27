88f5a5eb53229649507c71765131a8e8ead86ad0
Isaac Kim <myong328@gmail.com>
2020-03-27 11:08:09 -0700

    alertView and pickerView presentingaaaad

--- imagePicker/imagePicker/ViewController.swift
+++ imagePicker/imagePicker/ViewController.swift
@@ -16,5 +16,37 @@ class ViewController: UIViewController {
     }
 
 
+    
+    @IBAction func experiment() {
+        let nextController = UIImagePickerController()
+        present(nextController, animated: true, completion: nil)
+        
+    }
+    
+    
+    @IBAction func imagePicker(_ sender: Any) {
+        let image = UIImage()
+        let controller = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        self.present(controller, animated: true, completion: nil)
+        
+    }
+    
+    
+    @IBAction func alertView(_ sender: Any) {
+        let controller = UIAlertController()
+        controller.title = "Test Alert"
+        controller.message = "This is a test message"
+        
+        let okAction = UIAlertAction(title: "ok", style: .default) { (action) in
+            self.dismiss(animated: true, completion: nil)
+            
+        }
+        controller.addAction(okAction)
+        self.present(controller, animated: true, completion: nil)
+        
+        
+    }
+    
+    
 }
 
